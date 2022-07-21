7ffbe2420bea084b5acfb8e8891e7cc8d61b28a0
Joseph Broome <josephlloydbroome@gmail.com>
2022-07-20 20:29:42 -0700

    updates

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BankEncapsulation;
+
+BankAccount account = new BankAccount();
+
+Console.WriteLine("How much would you like to deposit?");
+
+var amountToDeposit= double.Parse(Console.ReadLine());
+
+account.Deposit(amountToDeposit);
+
+Console.WriteLine($"Thank you your balance is now {account.GetBalance()}");
